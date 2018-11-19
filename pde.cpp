05d246de0c638d133bc1b45daf6fbb32f1a7c100
mjdelgado10 <42391158+mjdelgado10@users.noreply.github.com>
2018-11-19 12:39:57 -0800

    Add files via upload
    Implementacion y generacion de los 3 casos, se utilizan varios ciclos para determinar las condiciones de frontera, se inicializan las condiciones de frontera y se impren los archivos que almacenan los datos de cada condicion.
    Se utiliza for, cout , ofstream...

--- pde.cpp
+++ pde.cpp
@@ -96,13 +96,13 @@ void condiciones_fronteras_fijas(float puntos[20][20])
 {
 // Se almacena las condiciones de frontera fijas en la Matriz
 
-	for (int i = 0; i < N; i++)
-	{
-		puntos[i][0] = 10.0;
-		puntos[0][i] = 10.0;
-		puntos[i][N-1] = 10.0;
-		puntos[N-1][i] = 10.0;
-	}
+for (int i = 0; i < N; i++)
+{
+	puntos[i][0] = 10.0;
+	puntos[0][i] = 10.0;
+	puntos[i][N-1] = 10.0;
+	puntos[N-1][i] = 10.0;
+}
 }
 
 //CASO 2; Condiciones de frontera abiertas
@@ -110,42 +110,177 @@ void condiciones_fronteras_fijas(float puntos[20][20])
 void condiciones_fronteras_abiertas(float puntos[20][20])
 {// Almacena las condiciones de frontera fijas en la Matriz
 
-	for (int i = 0; i < N; i++)
+for (int i = 0; i < N; i++)
+{
+	puntos[i][0] = puntos[i][1];
+	puntos[0][i] = puntos[1][i];
+	puntos[i][N-1] = puntos[i][N-2];
+	puntos[N-1][i] = puntos[N-2][i];
+}
+}
+
+//CASO 3.Condiciones de frontera periodicas
+void condiciones_fronteras_periodicas(float puntos[20][20], float calcita[20][20])
+{// Almacena las condiciones de frontera fijas en la matriz de calcita
+
+// las posiciones de temperatura los puntos vecinos
+float tfu;
+// puntos  de la temperatura(posiciones) pasados
+float tpa;
+
+int j_borde;
+int i_borde;
+int i, j;
+
+//ciclo cuando  j borde es 0
+j_borde = 0;
+for (i = 1; i < N-1; i++)
+{
+	tpa = puntos[i][j_borde] - ce*4*puntos[i][j_borde];
+	tfu = ce*( puntos[i-1][j_borde] + puntos[i+1][j_borde] + puntos[i][N-1] + puntos[i][j_borde+1]);
+	calcita[i][j_borde] = tpa + tfu;
+}
+
+//ciclo cuando j borde es 19
+j_borde = N-1;
+for (i = 1; i < N-1; i++)
+{
+	tpa = puntos[i][j_borde] - ce*4*puntos[i][j_borde];
+	tfu = ce*( puntos[i-1][j_borde] + puntos[i+1][j_borde] + puntos[i][j_borde-1] + puntos[i][0]);
+	calcita[i][j_borde] = tpa + tfu;
+}
+
+//ciclo cuando i borde es 0
+i_borde = 0;
+for (j = 1; i < N-1; j++)
+{
+	tpa = puntos[i_borde][j] - ce*4*puntos[i_borde][j];
+	tfu = ce*( puntos[N-1][j] + puntos[i_borde+1][j] + puntos[i_borde][j-1] + puntos[i_borde][j+1]);
+	calcita[i_borde][j] = tpa + tfu;
+}
+
+//ciclo cuando i borde es 19
+i_borde = N-1;
+for (j = 1; i < N-1; j++)
+{
+	tpa = puntos[i_borde][j] - ce*4*puntos[i_borde][j];
+	tfu = ce*( puntos[i_borde-1][j] + puntos[0][j] + puntos[i_borde][j-1] + puntos[i_borde][j+1]);
+	calcita[i_borde][j] = tpa + tfu;
+}
+
+// esquina
+i=0;
+j=0;
+tpa = puntos[i][j] - ce*4*puntos[i][j];
+tfu = ce*( puntos[N-1][j] + puntos[i+1][j] + puntos[i][N-1] + puntos[i][j+1]);
+calcita[i][j] = tpa + tfu;
+
+// esquina
+i=0;
+j=N-1;
+tpa = puntos[i][j] - ce*4*puntos[i][j];
+tfu = ce*( puntos[N-1][j] + puntos[i+1][j] + puntos[i][j-1] + puntos[i][0]);
+calcita[i][j] = tpa + tfu;
+
+// esquina
+i=N-1;
+j=0;
+tpa = puntos[i][j] - ce*4*puntos[i][j];
+tfu = ce*( puntos[i-1][j] + puntos[0][j] + puntos[i][N-1] + puntos[i][j+1]);
+calcita[i][j] = tpa + tfu;
+
+// esquina
+i=N-1;
+j=N-1;
+tpa = puntos[i][j] - ce*4*puntos[i][j];
+tfu = ce*( puntos[i-1][j] + puntos[0][j] + puntos[i][j-1] + puntos[i][0]);
+calcita[i][j] = tpa + tfu;
+}
+
+//guardar los tres archivos, uno por cada condicion de frontera
+void imprimir_datos(float puntos[20][20], char *nombre_archivo)
+{
+int i, j;
+ofstream imprimir_archivo(nombre_archivo);
+
+for (i = 0; i < N; i++)
+{
+	for (j = 0; j < N; j++)
 	{
-		puntos[i][0] = puntos[i][1];
-		puntos[0][i] = puntos[1][i];
-		puntos[i][N-1] = puntos[i][N-2];
-		puntos[N-1][i] = puntos[N-2][i];
+		imprimir_archivo << puntos[i][j] << " ";
 	}
+	imprimir_archivo << "\n";
+}
+
 }
 
 int main()
 {
-	//Condiciones de fontera fijas
-	float fronfijas[20][20];
-	float fronfijas_1[20][20];
-	//condiciones de fontera abiertas
-	float fronabiertas[20][20];
-	float fronabiertas_1[20][20];
 
-	// difusion de los puntos
-	float v=1.62/(820*2.71*1000); 
-	float dtt=pow(delta_x,2)*ce/v;
+
+//Condiciones de fontera fijas
+float fronfijas[20][20];
+float fronfijas_1[20][20];
+//condiciones de fontera abiertas
+float fronabiertas[20][20];
+float fronabiertas_1[20][20];
+//condiciones de fontera periodicas
+float fronperiodicas[20][20];
+float fronperiodicas_1[20][20];
+// difusion de los puntos
+float v=1.62/(820*2.71*1000); 
+float dtt=pow(delta_x,2)*ce/v;
 	
-	// Solucion para condiciones de frontera fijas
-	int i, j, n;
-	int nmax=50;
+// Solucion para condiciones de frontera fijas
+int i, j, n;
+int nmax=50;
 
 
-	// Incializar sistemas
-	tempinicial(fronfijas);
-	tempinicial(fronfijas_1);
-	// Para las condiciones de frontera fijas, inicializar los valores de las fronteras
-	condiciones_fronteras_fijas(fronfijas); 
+// Incializar sistemas
+tempinicial(fronfijas);
+tempinicial(fronfijas_1);
+// Para las condiciones de frontera fijas, inicializar los valores de las fronteras
+condiciones_fronteras_fijas(fronfijas); 
+condiciones_fronteras_fijas(fronfijas_1);
+// Para las condiciones de frontera abiertas, inicializar los valores de las fronteras
+tempinicial(fronabiertas);
+tempinicial(fronabiertas_1);
+// Para las condiciones de frontera periodicas, inicializar los valores de las fronteras
+tempinicial(fronperiodicas);
+tempinicial(fronperiodicas_1);
+
+for (n = 0; n < nmax; ++n)
+{
+	temperatura(fronfijas, fronfijas_1);
+	temperatura(fronabiertas, fronabiertas_1);
+	temperatura(fronperiodicas, fronperiodicas_1);
+
+	// Calcular condiciones de frontera
 	condiciones_fronteras_fijas(fronfijas_1);
-	// Para las condiciones de frontera abiertas, inicializar los valores de las fronteras
-	tempinicial(fronabiertas);
-	tempinicial(fronabiertas_1);
+	condiciones_fronteras_abiertas(fronabiertas_1);
+	condiciones_fronteras_periodicas(fronperiodicas , fronperiodicas_1);
+
+	// Guardar los valores nuevos en el arreglo 0
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			fronfijas[i][j] = fronfijas_1[i][j];
+			fronabiertas[i][j] = fronabiertas_1[i][j];
+			fronperiodicas[i][j] = fronperiodicas_1[i][j];
+		}
+	}
+
+	}
+
+//imprime los archivos por cada condicion de frontera
+imprimir_datos(fronfijas_1, "fronterasfijas.txt");
+imprimir_datos(fronabiertas_1, "fronterasabiertas.txt");
+imprimir_datos(fronperiodicas_1, "fronterasperiodicas.txt");
+	
+	
+
+cout << "Finalizar" << endl;
 
 return 0;
 }
